456605aab690aea9f07dc9f5e704c6d570f3ce5f
Renee Mumbi <1723renee@gmail.com>
2022-11-20 14:47:24 +0300

    Completed assignment

--- test/index-test.js
+++ test/index-test.js
@@ -37,11 +37,11 @@ describe('logWhisper(string)', function() {
 
 describe('sayHiToHeadphonedRoommate(string)', function() {
   it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToHeadphonedRoommate('hello')).toEqual("I can't hear you!");
+    expect(sayHiToHeadphonedRoommate(`i can\ 't hear you!`)).toEqual(`I can\ 't hear you!`);
   })
 
   it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToHeadphonedRoommate('HELLO')).toEqual("YES INDEED!");
+    expect(sayHiToHeadphonedRoommate('HELLO')).toEqual(`YES INDEED!`);
   })
 
   it('returns "I would love to!" if `string` is "Let\'s have dinner together!"`', function () {
